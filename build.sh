357acca7183cfb3de27fa174fc05edf04da3af91
Sandeep <sandeep@opstree.com>
2023-10-02 22:08:47 +0530

    Added capability to add mulitple policies

--- build.sh
+++ build.sh
@@ -11,16 +11,20 @@ sleep  $SLEEP_DURATION
 TASK_STATUS=0
 
 AWS_ACCOUNT_ID=`getAccountId`
-POLICY_ARN="arn:aws:iam::${AWS_ACCOUNT_ID}:policy/${POLICY_NAME}"
+IFS=","
+for POLICY_NAME in ${POLICIES}
+do
+    POLICY_ARN="arn:aws:iam::${AWS_ACCOUNT_ID}:policy/${POLICY_NAME}"
 
-POLICY_EXISTS=`policyExists ${POLICY_ARN}`
+    POLICY_EXISTS=`policyExists ${POLICY_ARN}`
 
-if [ "$POLICY_EXISTS" -eq 0 ]
-then
-    logWarningMessage "Policy with ARN ${POLICY_ARN} doesn't exist please check"
-else
-    logInfoMessage "Associating Policy with ARN ${POLICY_ARN} to role [$ROLE_NAME]"
-    aws iam attach-role-policy --policy-arn ${POLICY_ARN} --role-name ${ROLE_NAME}
-fi
+    if [ "$POLICY_EXISTS" -eq 0 ]
+    then
+        logWarningMessage "Policy with ARN ${POLICY_ARN} doesn't exist please check"
+    else
+        logInfoMessage "Associating Policy with ARN ${POLICY_ARN} to role [$ROLE_NAME]"
+        aws iam attach-role-policy --policy-arn ${POLICY_ARN} --role-name ${ROLE_NAME}
+    fi
+done
 
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
